d42ae4b999ed3baf4f4fd531363c2718e0a99a5d
pvijayavardhna@gmail.com <pvijayavardhan@gmail.com>
2026-07-24 18:43:13 +0530

    modify configratiion in mcp server setup for production

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ api.mount(
 
 if __name__ == "__main__":
    import uvicorn
-   #uvicorn.run("main:api", host="127.0.0.1", port=5000, reload=True)
+   # uvicorn.run("main:api", host="127.0.0.1", port=5000, reload=True)
    uvicorn.run("main:api", host="0.0.0.0", port=8000)
 
 
