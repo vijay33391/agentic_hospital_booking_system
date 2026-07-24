f106bdc9a47d2f9c7471d39204c6ca54584c2bc1
pvijayavardhna@gmail.com <pvijayavardhan@gmail.com>
2026-07-24 13:32:10 +0530

    change config to production

--- main.py
+++ main.py
@@ -197,9 +197,12 @@ api.mount(
 )
 
 
-''''if __name__ == "__main__":
-   
-    import uvicorn
 
-    
-    uvicorn.run("main:api", host="127.0.0.1", port=5000, reload=True)''''
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app:api",     
+        host="0.0.0.0",
+        port=8000,
+        
+    )
